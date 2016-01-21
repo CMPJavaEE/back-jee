3a5a749dd0596fe360beca9b8ffeb95de744969a
jee <jee@jee.jee>
2016-01-21 17:31:31 +0100

    Added static files

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,11 +2,24 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @SpringBootApplication
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry
+	      .addResourceHandler("/static/**")
+	      .addResourceLocations("/resources/app/")
+	      .setCachePeriod(3600)
+	      .resourceChain(true)
+	      .addResolver(new PathResourceResolver());
 	}
 }

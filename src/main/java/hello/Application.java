776687eb402b81b7148a254000c1e00bf6b7aadb
jee <jee@jee.jee>
2016-01-23 17:34:25 +0100

    Move from java configuration to application.properties, also switch from in memory database to postgresql with schema auto update

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,24 +2,12 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @SpringBootApplication
-public class Application extends WebMvcConfigurerAdapter {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	    registry
-	      .addResourceHandler("/static/**")
-	      .addResourceLocations("/resources/app/")
-	      .setCachePeriod(3600)
-	      .resourceChain(true)
-	      .addResolver(new PathResourceResolver());
-	}
 }

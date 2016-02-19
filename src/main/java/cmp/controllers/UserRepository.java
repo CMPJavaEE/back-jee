59197b492a8ffb4bd12285015b30cf2deb8098ff
archloy <Archloy@users.noreply.github.com>
2016-02-19 19:28:36 +0100

    FIX : UserRepository findby changes after rename User.nick propertie to login

--- src/main/java/cmp/controllers/UserRepository.java
+++ src/main/java/cmp/controllers/UserRepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
-	List<User> findByNick(@Param("nick") String name);
+	List<User> findByLogin(@Param("login") String name);
 
 }
